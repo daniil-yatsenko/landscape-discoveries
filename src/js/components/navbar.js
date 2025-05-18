080ccd4f82cdf3d9ed9ec0fcb5c6d1da33f9a146
Daniil <daniil@notdiy.studio>
2025-05-18 19:23:18 +0200

    current link highlight

---
+++ src/js/components/navbar.js
@@ -0,0 +1,35 @@
+const activeLink = (data) => {
+  const navbar = document.querySelector(".navbar_navbar");
+  const navLinks = navbar.querySelectorAll(".navbar_navlink");
+  const namespace = data.next.namespace;
+
+  if (!namespace) return;
+
+  // first word of link's text content
+  const namesToHref = {
+    Home: ["/", "/home-v2"],
+    About: ["/about"],
+    Services: ["/services-processes"],
+    Gallery: ["/gallery"],
+    Plants: ["/plants"],
+  };
+
+  navLinks.forEach((navLink) => {
+    const linkName = navLink.textContent.trim().split(" ")[0];
+    const href = navLink.getAttribute("href");
+    const pathname = window.location.pathname;
+
+    navLink.classList.remove("w--current");
+    console.log(linkName, href);
+
+    if (namesToHref[linkName]?.includes(href) && href === pathname) {
+      navLink.classList.add("w--current");
+    }
+
+    if (pathname === "/home-v2" && linkName === "Home") {
+      navLink.classList.add("w--current");
+    }
+  });
+};
+
+export { activeLink };
